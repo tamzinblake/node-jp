4767edb2288baa014f143b01c3e8277daea984dd
Thom Blake <tblake@brightroll.com>
2014-05-08 13:13:14 -0700

    Implement basic functionality

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,1 +1,25 @@
 #!/usr/bin/env node
+/* global process require */
+
+var util = require('util')
+
+var data = ''
+
+process.stdin.setEncoding('utf8')
+
+process.stdin.on('readable' ,function () {
+  var chunk = process.stdin.read()
+  if (chunk !== null) {
+    data += chunk
+  }
+})
+
+process.stdin.on('end' ,function () {
+  try {
+    var json = JSON.parse(data)
+    process.stdout.write(util.format('%j' ,json))
+  }
+  finally {
+    process.stdout.write('\n')
+  }
+})
